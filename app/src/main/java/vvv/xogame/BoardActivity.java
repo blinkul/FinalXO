1a6c88b37ad02f8c30d598481640229c85549125
blinkul <anghelciprian91@gmail.com>
2017-08-20 17:43:59 +0300

    Corrected winning

--- app/src/main/java/vvv/xogame/BoardActivity.java
+++ app/src/main/java/vvv/xogame/BoardActivity.java
@@ -149,26 +149,6 @@ public class BoardActivity extends AppCompatActivity {
     private boolean isWinning(int[] playerBoard){
         boolean status = false;
 
-//        int[][] winningOptions = {
-//                {1,1,1,0,0,0,0,0,0},
-//                {0,0,0,1,1,1,0,0,0},
-//                {0,0,0,0,0,0,1,1,1},
-//                {1,0,0,1,0,0,1,0,0},
-//                {0,1,0,0,1,0,0,1,0},
-//                {0,0,1,0,0,1,0,0,1},
-//                {1,0,0,0,1,0,0,0,1},
-//                {0,0,1,0,1,0,1,0,0}
-//        };
-//
-//        for(int i = 0; i < winningOptions.length; i++) {
-//
-//
-//            if (Arrays.equals(playerBoard, winningOptions[i])) {
-//                status = true;
-//            }
-//
-//        }
-
         // 0 1 2
         // 3 4 5
         // 6 7 8
